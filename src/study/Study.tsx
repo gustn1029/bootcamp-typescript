ee309fa8039cfdf3c8b4e4ce45baf12dea8af8e5
hs0820 <gustn1029@gmail.com>
2024-09-09 15:50:50 +0900

    240909 타입스크립트 강의 코드

--- src/study/Study.tsx
+++ src/study/Study.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 
 const Study = () => {
-  function add<T>(a: T, b: T): T {
-    if (typeof a === "number" && typeof b === "number") {
-      return (a + b) as T;
-    } else {
-      return (a as string + b) as T;
+  document.querySelector("input")!.addEventListener("onChange", (e:Event) => {
+    const target = e.target;
+    if(target && target instanceof HTMLInputElement) {
+      console.log(target.value);
     }
-  }
-
-  const addedString = add("hello ", "world"); //문자열로 타입추론되도록.
-  const addedNum = add(3, 5);
-  // 여기서는 숫자로
-
-  console.log(addedString);
-  console.log(addedNum);
+  });
 
   return (
     <>
-      <h1>20240906 강의</h1>
+      <h1>20240909 강의</h1>
     </>
   );
 };
