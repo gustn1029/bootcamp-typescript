a4a4d506131a1f5249a801ab40f02e8d6bb10f42
hs0820 <gustn1029@gmail.com>
2024-09-12 18:15:56 +0900

    study 초기화

--- src/study/Study.tsx
+++ src/study/Study.tsx
@@ -1,16 +1,9 @@
-import React from "react";
 
 const Study = () => {
-  document.querySelector("input")!.addEventListener("onChange", (e:Event) => {
-    const target = e.target;
-    if(target && target instanceof HTMLInputElement) {
-      console.log(target.value);
-    }
-  });
-
+ 
   return (
     <>
-      <h1>20240909 강의</h1>
+      <h1>202409 강의</h1>
     </>
   );
 };
